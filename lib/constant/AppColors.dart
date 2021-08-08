de1af6036fe4e4e52d2bf305a17aaa7d11fe541c
Md Abu Sayeed <abusayeed33343536@gmail.com>
2021-08-09 01:58:04 +0600

    Bottom Navigation

---
+++ lib/constant/AppColors.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+import 'package:flutter/cupertino.dart';
+
+class AppColors{
+  static const Color dark_red = Color(0xff8b0000);
+}
---
+++ lib/ui/splash_screen.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+
+import 'package:firstapp/constant/AppColors.dart';
+import 'package:firstapp/ui/login_screen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Timer(Duration(seconds: 3),()=>Navigator.push(context, CupertinoPageRoute(builder: (_)=>LoginScreen())));
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.dark_red,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text("FishHut",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 44.sp),),
+              SizedBox(height: 20.h,),
+              CircularProgressIndicator(color: Colors.white,),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
